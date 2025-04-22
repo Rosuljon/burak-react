0ca53e5cee048227d1f8e14ad956551a01f593e8
Rosuljon <ikramjanov97@mail.ru>
2025-04-22 12:23:54 +0900

    fix: organize homepage sectional components

---
+++ src/app/screens/homePage/ActiveUsers.tsx
@@ -0,0 +1,12 @@
+import { Container } from "@mui/material";
+import React from "react";
+
+const ActiveUsers = () => {
+  return (
+    <div>
+      <Container>ActiveUsrs</Container>
+    </div>
+  );
+};
+
+export default ActiveUsers;
--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,5 +1,20 @@
-import { Container } from "@mui/material";
+import Statistics from "./Statistics";
+import PopularDishes from "./PopularDishes";
+import NewDishes from "./NewDishes";
+import Ads from "./Ads";
+import ActiveUsers from "./ActiveUsers";
+import Events from "./Events";
+import "../../../css/home.css";
 
 export function HomePage() {
-  return <Container>Home</Container>;
+  return (
+    <div className="homepage">
+      <Statistics />
+      <PopularDishes />
+      <NewDishes />
+      <Ads />
+      <ActiveUsers />
+      <Events />
+    </div>
+  );
 }
