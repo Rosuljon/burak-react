f88f7b4108e4f9be853f3b56c8a33090d506c62c
Rosuljon <ikramjanov97@mail.ru>
2025-04-23 14:02:19 +0900

    feat: develop ads sectional components

--- src/app/screens/homePage/Ads.tsx
+++ src/app/screens/homePage/Ads.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Ads = () => {
-  return <div>Ads</div>;
+  return (
+    <div className="ads-restaurant-frame">
+      <video
+        className="ads-video"
+        autoPlay={true}
+        loop
+        muted
+        playsInline
+        data-video-media=""
+      >
+        <source type="video/mp4" src="video/burak-ads.mp4" />
+      </video>
+    </div>
+  );
 };
 
 export default Ads;
